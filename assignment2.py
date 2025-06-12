5d448a0c92ff2aa09178d170e729d3b97883527f
Kesarahuja <ahujakesar@gmail.com>
2025-06-13 01:50:13 +0530

    updated

--- assignment2.py
+++ assignment2.py
@@ -9,10 +9,7 @@ class LinkedList:
         self.head = None 
 
     def add_node(self, data):
-        # adds a new node to end list.
         new_node=Node(data)
-
-        # if list is empty then new node becomes the head
         if not self.head:
             self.head = new_node
             return
@@ -35,3 +32,26 @@ class LinkedList:
     def delete_nth_node(self, n):
         if not self.head:
             raise Exception("List is empty. Nothing to delete.")
+            return
+
+        current = self.head
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+        print("None")  
+    
+        if n <= 0:
+            raise Exception("Invalid index. Must be 1 or greater.")
+
+        if n == 1:
+            self.head = self.head.next
+            return
+        current = self.head
+        count = 1
+        while current and count < n - 1:
+            current = current.next
+            count += 1
+
+        if not current or not current.next:
+            raise Exception("Index out of range. Cannot delete node.")
+        current.next = current.next.next
