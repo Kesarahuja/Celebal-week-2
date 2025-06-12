8c7216a2d24d5ed994c0a36c2d59fac449a1cc98
Kesarahuja <ahujakesar@gmail.com>
2025-06-13 00:53:52 +0530

    node class

---
+++ assignment2.py
@@ -0,0 +1,7 @@
+# node class represents a single element in the linked list
+class Node:
+    def __init__(self, data):
+        self.data=data    # actual data stored in node
+        self.next=None    # Pointer to node 
+
+
